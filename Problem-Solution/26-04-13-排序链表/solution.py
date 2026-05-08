5772021948c55593e67625f8a285582b4cec7304
Aegis Test <test@aegis-stack.dev>
2026-05-08 18:21:31 +0800

    keep updating

--- Problem-Solution/26-04-13-排序链表/solution.py
+++ Problem-Solution/26-04-13-排序链表/solution.py
@@ -14,9 +14,61 @@ class Solution:
                     temp1 = temp1.next
                 else:
                     temp.next = temp2
-                    temp2 = temp.next
+                    temp2 = temp2.next
                 temp = temp.next
             if temp1:
                 temp.next = temp1
             elif temp2:
-                    
+                temp.next = temp2
+            return dummyHead.next
+        
+        if not head:
+            return head
+
+        length = 0
+        node = head 
+        while node:
+            length += 1
+            node = node.next
+
+        dummyHead = ListNode(0 , head)
+        subLength = 1
+        while subLength < length:
+            prev , curr = dummyHead , dummyHead.next
+            while curr:
+                head1 = curr
+                for i in range(1 , subLength):
+                    if curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                head2 = curr.next
+                curr.next = None
+                curr = head2
+                for i in range(1 , subLength):
+                    if curr and curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                succ = None
+                if curr:
+                    succ = curr.next
+                    curr.next = None
+                
+                merged = merge(head1 , head2)
+                prev.next = merged
+                while prev.next:
+                    prev = prev.next
+                curr = succ
+            subLength <<= 1
+        return dummyHead.next
+    
+s = Solution()
+head = ListNode(4)
+head.next = ListNode(2)
+head.next.next = ListNode(1)
+head.next.next.next = ListNode(3)
+result = s.sortList(head)
+while result:
+    print(result.val)
+    result = result.next
